e3d16a5593e218e5fe22aff8cc47783108df98d5
Ivana Zuzic <izuzic3@gmail.com>
2018-06-09 15:40:43 +0200

    Glavna mreža i dataset
    Dodana glavna mreža koja klasificira slova i dataset za slova.

---
+++ glavnamreza.py
@@ -0,0 +1,88 @@
+from __future__ import print_function
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+img_rows, img_cols = 100, 100
+input_shape = (img_rows, img_cols, 1)
+num_classes = 33
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=False)
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        'data/letters/train',
+        target_size=(img_rows, img_cols),
+		color_mode='grayscale',
+        batch_size=120,
+        class_mode='categorical',
+		shuffle='True')
+
+validation_generator = val_datagen.flow_from_directory(
+        'data/letters/validation',
+        target_size=(img_rows, img_cols),
+		color_mode='grayscale',
+        batch_size=120,
+        class_mode='categorical',
+		shuffle='True')
+
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=120,
+        epochs=12,
+		verbose=1,
+        validation_data=validation_generator,
+        validation_steps=120)
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
